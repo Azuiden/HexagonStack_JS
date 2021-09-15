b5e426bae907044c0f96c2e3d2446125224cb11b
Azuiden <72172428+Azuiden@users.noreply.github.com>
2021-09-14 21:49:57 -0500

    Update HexagonStack_JS.js

--- HexagonStack_JS.js
+++ HexagonStack_JS.js
@@ -1,8 +1,9 @@
-let cols, rows, rowAdj, colAdj, cellWidth, cellHeight; //grid variables
-let radius, numPoints, translateY, xDiv, xDiv2, yDiv, vertSpaceAdj; //hexagon variables
-let stackNum, stackSmooth, stackHex, stackMatrix; //matrix/stack variables
+let cols, rows, cellWidth, cellHeight; //grid variables
+let radius, hexHoleRnd, translateY, vertSpaceAdj, HorzSpaceAdj; //hexagon variables
+let stackNum, stackSmooth, dataMatrix, stackMatrix, hexHoleMatrix; //matrix/stack variables
+
 ///COLORS///
-let bk;
+let bk, colorMatrix;
 let BK = ["#87A5A5", "#BFA8BE", "#D9896D", "#F26666", "#A0DEAE", "#40352C"];
 let Color1 = ["#8C3E37", "#5E608C", "#F29B88", "#D9C2AD", "#A64B63", "#537D91"]; //darker colors
 let Color2 = ["#BF573F", "#E9F0F2", "#F2CC85", "#F2D6B3", "#F2AB6D", "#DEA895"]; // lighter colors
@@ -16,15 +17,23 @@ function setup() {
   
   //Hexagon parameters
   radius = 70;
-  vertSpaceAdj = 1; //adjusts the space between columns
-  numPoints = 6;
+  vertSpaceAdj = 1.6; //adjusts space between columns; bigger number = more space
+  HorzSpaceAdj = 1.8; //adjusts space between rows
+  hexHoleRnd = 5;
   translateY = 20;
   stackNum = 7;
   stackSmooth = 200;
   
-  xDiv = 2;//default 2
-  xDiv2 = 2; //default 2
-  yDiv = 2; //default 2
+  cellWidth = radius * vertSpaceAdj;
+  cellHeight = radius * HorzSpaceAdj;
+  cols = int(displayWidth/cellWidth +1);
+  rows = int(displayHeight/cellHeight +1);
+  
+  dataMatrix = new Matrix();
+  colorMatrix = dataMatrix.colorMatrix;
+  hexHoleMatrix = dataMatrix.hexHoleMatrix;
+  stackMatrix = dataMatrix.stackMatrix;
+  stackMatrix = dataMatrix.runAverageMultiple(stackSmooth);
   
   noLoop();
 }
@@ -33,127 +42,119 @@ function setup() {
 function draw() {
   background(bk);
   
-  //add or subtract from these to control space between rows/columns
-  cols = round(windowWidth/(radius*2) +2);
-  rows = round(windowHeight/(radius*2) -1);
-  cellWidth = width/cols;
-  cellHeight = height/rows;
-  rowAdj = rows + round(vertSpaceAdj);
-  colAdj = cols; //might need later, this isnt a useless variable I promise
-  stackHex = new Matrix();
-  //printMatrix(stackHex.matrix);
-  //print("--------");
-  stackMatrix = stackHex.stackMatrix;
-  stackMatrix = stackHex.runAverageMultiple(stackSmooth);
-  //printMatrix(stackHex.matrix);
-  
-  for (let r = 0; r < rowAdj; r++){
-      for (let c = 0; c < colAdj; c++){ //EVEN COLUMNS
-        let rndColor = intRandom(Color1.length);
-        let color1 = Color1[rndColor];
-        let color2 = Color2[rndColor];
-        
-        let centerX = c * cellWidth + cellWidth/xDiv;
+  for (let r = 0; r < rows; r++){
+    
+      //EVEN COLUMNS
+      //Need to be drawn first so that the offset odd columns are drawn over them
+      for (let c = 0; c < cols; c++){ 
+        let centerX = c * cellWidth;
         let centerY = r * cellHeight;
         
-        //EVEN COLUMNS
         if (c % 2 == 0){ 
           print("row: " + r + " column: " + c);
-          drawAllHex(centerX, centerY, radius, color1, color2, c, r);
+          drawAllHex(centerX, centerY, radius, c, r);
         }
       }
       
-      for (let c = 0; c < colAdj; c++){ //ODD COLUMNS
-        let rndColor = intRandom(Color1.length);
-        let color1 = Color1[rndColor];
-        let color2 = Color2[rndColor];
-      
-        let centerX = c * cellWidth + cellWidth/xDiv2;
-        let centerY = r * cellHeight + cellHeight/yDiv;
+      for (let c = 0; c < cols; c++){ //ODD COLUMNS
+        let centerX = c * cellWidth;
+        let centerY = r * cellHeight + cellHeight/2;
         
-        //ODD COLUMNS
         if (c % 2 != 0){
          print("row: " + r + " column: " + c);
-         drawAllHex(centerX, centerY, radius, color1, color2, c, r);
+         drawAllHex(centerX, centerY, radius, c, r);
         }
       }
       
     }//end of rows for loop
 }//end of function draw()
 
-function drawAllHex(centerX, centerY, radius, color1, color2, c, r){
+function drawAllHex(centerX, centerY, radius, c, r){
+    let shadowColor = Color1[colorMatrix[r][c]];
+    let baseColor = Color2[colorMatrix[r][c]];
     //stacking the hexagons
     for (let i = 0; i < stackMatrix[r][c]; i++){
       if (i == 1){
-        fill(color1);
-        Polygon2(centerX, centerY , radius, translateY);
-        fill(color2);
-        Polygon(centerX, centerY, radius);
-        polyHole(centerX, centerY , radius, translateY, color1, bk);
+        fill(shadowColor);
+        HexagonShadow(centerX, centerY , radius, translateY);
+        fill(baseColor);
+        Hexagon(centerX, centerY, radius);
+        HexHole(centerX, centerY , radius, translateY, shadowColor, bk, hexHoleMatrix[r][c]);
       }
       else if (i > 1){
-        fill(color1);
-        Polygon2(centerX, centerY - (i+1 * translateY) , radius, translateY +3);
-        fill(color2);
-        Polygon(centerX, centerY - (i+1 * translateY), radius);
-        polyHole(centerX, centerY - (i+1 * translateY), radius, translateY, color1, color2);
+        fill(shadowColor);
+        HexagonShadow(centerX, centerY - (i+1 * translateY) , radius, translateY + 3);
+        fill(baseColor);
+        Hexagon(centerX, centerY - (i+1 * translateY), radius);
+        HexHole(centerX, centerY - (i+1 * translateY), radius, translateY, shadowColor, baseColor, hexHoleMatrix[r][c]);
       }
     }
 }//end of drawAllHex()
 
-function polyHole(centerX, centerY, radius, translateY, color1, fillColor){
-  if (random(5) < 1) //randomly add hole to hexagon
+function HexHole(centerX, centerY, radius, translateY, shadowColor, fillColor, hexChance){
+  if (hexChance == 1) //randomly add hole to hexagon
       {
         push();
-        translate(centerX, centerY+translateY/2); //translate so that 0,0 is center of shape
-        fill(color1);
+        translate(centerX, centerY + translateY/2); //translate so that 0,0 is center of shape
+        fill(shadowColor);
         rotate(PI); //rotate at 0,0
         scale(1.1,1);
-        Polygon2(0, 0, radius-(radius*0.5), translateY);
+        HexagonShadow(0, 0, radius-(radius/2), translateY);
         fill(fillColor);
-        Polygon(0, 0, radius-(radius*0.5));
+        Hexagon(0, 0, radius-(radius/2));
         pop();
       }
-}//end polyHole()
+}//end HexHole()
 
-function Polygon(centerX, centerY, radius) {
+function Hexagon(centerX, centerY, radius) {
   noStroke();
   beginShape();
   //While a < TWO_PI (full circumference of a circle)
   //Increment by angle value, which will increment for the number of verticies originally given
-  for (let a = 0; a < TWO_PI; a += TWO_PI / numPoints) {
+  for (let a = 0; a < TWO_PI; a += TWO_PI / 6) {
     let sx = centerX + cos(a) * radius;
     let sy = centerY + sin(a) * radius;
     vertex(sx, sy);
   }
   endShape(CLOSE);
-}
+}//end Hexagon()
 
-function Polygon2(centerX, centerY, radius, translateY) {
-  //creates a dropshadow  for polygon() by creating the same polygon offset by specified amount
+function HexagonShadow(centerX, centerY, radius, translateY) {
+  //creates a dropshadow  for Hexagon() by creating the same hexagon offset by specified amount
   //then drawing a rectangle 
- Polygon(centerX,centerY + translateY,radius); 
+ Hexagon(centerX, centerY + translateY, radius); 
  noStroke();
  //the 4 coordinates of the quad
  //Could plug these into the quad() but this is easier to read the point values for me
- let xy1 = [centerX + radius,centerY]; //TL
- let xy2 = [centerX + radius,centerY + translateY]; //TR
- let xy3 = [centerX - radius,centerY]; //BL
- let xy4 = [centerX - radius,centerY + translateY]; //BR
+ let xy1 = [centerX + radius, centerY]; //TL
+ let xy2 = [centerX + radius, centerY + translateY]; //TR
+ let xy3 = [centerX - radius, centerY]; //BL
+ let xy4 = [centerX - radius, centerY + translateY]; //BR
  quad(xy1[0],xy1[1],xy2[0],xy2[1],xy3[0],xy3[1],xy4[0],xy4[1]);
-}
+}//end HexagonShadow()
 
 /////////MATRIX CLASS/////////
 
 class Matrix{ //matrix class
 
   constructor(){ //constructor for generating random numbers in matrix
-    this.stackMatrix = new Array(rowAdj);
-    for (let r = 0; r < rowAdj; r++) {
-      this.stackMatrix[r] = new Array(colAdj);
-      for (let c = 0; c < colAdj; c++) {
+    this.stackMatrix = new Array(rows);
+    this.colorMatrix = new Array(rows);
+    this.hexHoleMatrix = new Array(rows);
+    
+    for (let r = 0; r < rows; r++) {
+      this.stackMatrix[r] = new Array(cols);
+      this.colorMatrix[r] = new Array(cols);
+      this.hexHoleMatrix[r] = new Array(cols);
+      
+      for (let c = 0; c < cols; c++) {
         let rndNum = intRandom(stackNum);
+        let rndColor = intRandom(Color1.length);
+        let rndHole = intRandom(hexHoleRnd);
+        
         this.stackMatrix[r][c] = rndNum;
+        this.colorMatrix[r][c] = rndColor;
+        this.hexHoleMatrix[r][c] = rndHole;
       }//end column
     }//end row
   }//end of Matrix(rows,cols)
@@ -166,8 +167,8 @@ class Matrix{ //matrix class
   }
   
   AverageValues(){
-    let rndRow = intRandom(rowAdj);
-    let rndCol = intRandom(colAdj);
+    let rndRow = intRandom(rows);
+    let rndCol = intRandom(cols);
     //print("rndRow: " + rndRow)
   
     //all the indexes that need to be checked
@@ -185,7 +186,7 @@ class Matrix{ //matrix class
     
     for (let i = 0; i < 6; i++){
       //checking for out of bounds values
-      if ((searchMatrix[i][0] >= 0 && searchMatrix[i][0] < rowAdj) && (searchMatrix[i][1] >= 0 && searchMatrix[i][1] < colAdj)){
+      if ((searchMatrix[i][0] >= 0 && searchMatrix[i][0] < rows) && (searchMatrix[i][1] >= 0 && searchMatrix[i][1] < cols)){
         //checks if searched value is lower, higher, or the same as the selected value and tallies result
         if (this.stackMatrix[searchMatrix[i][0]][searchMatrix[i][1]] < this.stackMatrix[rndRow][rndCol]){
           low++;
@@ -220,11 +221,7 @@ class Matrix{ //matrix class
    }
    
    function intRandom(input){
-     let result = round(random(input));
-     if (result >= input){
-      result = input - 1; 
-     }
-     return result;
+     return int(random(input));
    }
    
    function windowResized(){
